9ab9faaeb98011f01f767dddc44ec60902a7e351
Yaroslav Matiash <nice.matyash99@gmail.com>
2024-02-18 20:19:28 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-function slugify(title) {
-  const words = title.toLowerCase().split(' ');
-  return words.join('-');
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+  for (let product in products) {
+    totalProducts += products[product];
+  }
+
+  return totalProducts <= containerSize;
 }
 
-console.log(slugify('Arrays for begginers'));
-console.log(slugify('English for developer'));
-console.log(slugify('Ten secrets of JavaScript'));
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
